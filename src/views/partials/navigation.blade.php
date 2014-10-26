f458d76a4bb112d645a6f70828f417b141c9ef30
jayaregalinada <jayaregalinada@gmail.com>
2014-10-26 13:32:13 +0800

    Fixed template for header and navigation

--- src/views/partials/navigation.blade.php
+++ src/views/partials/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="_widget _widget_navigation" data-widget-name="navigation">
-    <div class="_widget_container container-fluid clearfix">
+    <div class="_widget_container row clearfix">
         @foreach( \Config::get('walis-t-buyandsell::template.navigations') as $key => $value )
-        <div class="text-center col-xs-6 col-md-3 nav_1" data-nav="{{ $value }}">
+        <div class="text-center column col-xs-6 col-md-3" data-nav="{{ $value }}">
             @if( $navigation === $value )
             <a href="#" class="active content">
             @else
--- src/views/widgets/header.blade.php
+++ src/views/widgets/header.blade.php
@@ -1,24 +1,15 @@
-<div class="_widget _widget_header clearfix">
+<div class="_widget _widget_header row clearfix">
     {{-- LOGO --}}
     <div class="_widget _widget_home-banner col-md-10 clearfix cf">
-        <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/logo.jpg') }}" alt="LOGO" class="animated fadeInDown ceter-block img-responsive" />
+        <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/logo.jpg') }}" alt="LOGO" class="animated fadeInDown center-block img-responsive" />
     </div>
     {{-- /LOGO --}}
     <div class="_widget _widget_custom accept-payment col-md-2" data-class="accept-payment" data-widget="custom">
-        <p>Buy Online or Call us on:<br />
+        <p>Buy Online or Call us on:<br class="hidden-xs hidden-sm" />
         GLOBE / SMART</p>
         <p>We accept payment via</p>
-        <div class="clearfix logos">
-            <div class="col-xs-12 text-center">
-                <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/paypal.jpg') }}" class="center-block text-center" />
-            </div>
-            <div class="col-xs-6 text-center">
-                <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/lbc.jpg') }}" alt="lbc" class="center-block text-center" />
-            </div>
-            <div class="col-xs-6 text-center">
-                <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/western.jpg') }}" alt="" class="center-block text-center" />
-            </div>
-
+        <div class="clearfix row logos">
+            <img src="{{ asset('packages/walisph/walis-template-buyandsell/images/payment-method.jpg') }}" class="center-block text-center" />
         </div>
     </div>
 
